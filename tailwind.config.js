e0925b2e999dcd799f8a52594732bf0938178c6e
Pedro <pedro.phprado@gmail.com>
2023-12-08 15:59:53 -0300

    Feat: Inicio do portfolio 2024: mobile

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*{.html, js}"],
+  theme: {
+    extend: {      
+      fontFamily:{
+      nunito: ['Nunito', 'sans-serif']
+    },
+  },
+  },
+  plugins: [],
+}
+
